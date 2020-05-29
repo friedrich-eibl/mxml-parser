11e616566d90912b98fe7e15ba938c5c8bef7f67
Friedrich Eibl <friedrich.eibl@hu-berlin.de>
2020-05-30 00:47:22 +0200

    fixed wrong file extensions in test cases

--- parser.py
+++ parser.py
@@ -3,8 +3,8 @@ import xml.sax
 ################################
 # TESTCASES:
 #xmlFile = 'test.mxml' 			# --- works
-xmlFile = 'test2.mxml'			# --- X
-#xmlFile = 'test3.mxml'			# --- X
+#xmlFile = 'test2.xml'			# --- X
+xmlFile = 'test3.xml'			# --- X
 ################################
 
 #################################
