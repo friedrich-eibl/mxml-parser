c61489539051745fae76aaadb7766feba69cb2f7
Friedrich Eibl <friedrich.eibl@hu-berlin.de>
2020-05-30 00:43:27 +0200

    first build of basic parser

--- parser.py
+++ parser.py
@@ -1,5 +1,12 @@
 import xml.sax
 
+################################
+# TESTCASES:
+#xmlFile = 'test.mxml' 			# --- works
+xmlFile = 'test2.mxml'			# --- X
+#xmlFile = 'test3.mxml'			# --- X
+################################
+
 #################################
 # This is an mxml-parser	#
 # (more information in README)	#
@@ -8,15 +15,28 @@ import xml.sax
 
 class sheetHandler(xml.sax.ContentHandler):
 	def __init__(self):
+		self.currcont = ""
+		self.name = ""
 
 	#start element
 	def startElement(self, tag, attributes):
+		self.currcont = tag
+		if tag == 'note':
+			print('++++ Note ++++')
+
 
 	#end element
 	def endElement(self, tag):
+		if self.currcont == 'step':
+			print(' Step: ' + self.name)
 
 	#characters
 	def characters(self, content):
+		if self.currcont == 'step':
+			self.name = content
 
 parser = xml.sax.make_parser()
 handler = sheetHandler()
+
+parser.setContentHandler(handler)
+parser.parse(xmlFile)
